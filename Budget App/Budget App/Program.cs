5e61017f7baef566042ae7a0945bfc2c67cae68f
Jacob Smith <jacobsmith619@gmail.com>
2023-11-23 16:30:55 -0500

    Created menu in program.cs and added Expense.cs

--- Budget App/Budget App/Program.cs
+++ Budget App/Budget App/Program.cs
@@ -1,3 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+Console.WriteLine("----------");
+Console.WriteLine("BUDGET APP");
+Console.WriteLine("----------");
+Console.WriteLine("Press '1' to add an expense");
+Console.WriteLine("Press '2' to view expenses");
+Console.WriteLine("Press '3' to remove an expense");
+Console.WriteLine("Type 'exit' to quit");
+
+string menuAction = Console.ReadLine();

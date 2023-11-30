fef6cf2b8db1a91c0af72853170172489a88dcfa
Jacob Smith <jacobsmith619@gmail.com>
2023-11-30 07:32:53 -0500

    Added Unit Test Project to solution

---
+++ Budget App/BudgetApp.Tests/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using Budget_App;
+
+namespace BudgetApp.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void AddExpense_IncreasesExpenseCount()
+    {
+        ExpenseLogic expense = new ExpenseLogic();
+        int initialCount = expense.GetExpenses().Count;
+
+        expense.AddExpense();
+
+        Assert.Equal(initialCount + 1, expense.GetExpenses().Count);
+    }
+
+    [Fact]
+    public void ViewExpense_ReturnsExpenseList()
+    {
+        ExpenseLogic expense = new ExpenseLogic();
+
+        var expenses = expense.GetExpenses();
+
+        Assert.NotNull(expenses);
+        Assert.IsType<List<Expense>>(expenses);
+    }
+
+    [Fact]
+    public void RemoveExpense_ReducesExpenseCount()
+    {
+        ExpenseLogic expense = new ExpenseLogic();
+        expense.AddExpense();
+        int initialCount = expense.GetExpenses().Count;
+
+        expense.RemoveExpenses();
+
+        Assert.Equal(initialCount - 1, expense.GetExpenses().Count);
+    }
+}
